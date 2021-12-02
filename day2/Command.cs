e55e7a1e3ce9939e976467b722e1e348dea1557d
Matt Sharpe <mds@live.co.uk>
2021-12-02 21:03:53 +0000

    Tidying

--- day2/Command.cs
+++ day2/Command.cs
@@ -4,18 +4,22 @@ namespace Aoc2021.Day2
 {
     internal class Command
     {
-        public Direction Direction { get; set; }
-        public int Units { get; set; }
+        public Command(Direction direction, int units)
+        {
+            Direction = direction;
+            Units = units;
+        }
+
+        public Direction Direction { get; }
+        public int Units { get; }
 
         public static Command Parse(string s)
         {
             var parts = s.Split(' ');
 
-            return new Command
-            {
-                Direction = (Direction)Enum.Parse(typeof(Direction), parts[0], ignoreCase: true),
-                Units = int.Parse(parts[1])
-            };
+            return new Command(
+                (Direction)Enum.Parse(typeof(Direction), parts[0], ignoreCase: true),
+                int.Parse(parts[1]));
         }
     }
 }
