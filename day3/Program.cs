3070e06455c12e996ca5c1f00910e5d733641b54
Matthew Sharpe <matthew.sharpe@enable.com>
2021-12-03 12:12:46 +0000

    d3p1 wip

---
+++ day3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aoc2021.Day3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines(args[0]);
+        }
+    }
+}
