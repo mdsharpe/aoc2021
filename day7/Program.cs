af03202c008dd2b1571939d57b569827b4c08fa1
Matthew Sharpe <matthew.sharpe@enable.com>
2021-12-07 12:52:14 +0000

    d7p2

--- day7/Program.cs
+++ day7/Program.cs
@@ -11,16 +11,34 @@ namespace Aoc2021.Day7
                 .Select(int.Parse)
                 .ToArray();
 
+            var constantRate = args.Length > 1 && args.Any(o => string.Equals("cr", o, StringComparison.OrdinalIgnoreCase));
+
             var x = from i in Enumerable.Range(crabLocations.Min(), crabLocations.Max() - crabLocations.Min())
                     select new
                     {
                         Location = i,
-                        Fuel = crabLocations.Select(o => Math.Abs(o - i)).Sum()
+                        Fuel = CalculateFuel(crabLocations, i, constantRate)
                     };
 
             var bestLocation = x.OrderBy(o => o.Fuel).First();
 
             Console.WriteLine($"Location {bestLocation.Location} costs a total of {bestLocation.Fuel} fuel.");
+        }
+
+        private static int CalculateFuel(int[] crabLocations, int targetLocation, bool constantRate)
+        {
+            int fuel;
+
+            if (constantRate)
+            {
+                fuel = crabLocations.Select(o => Math.Abs(o - targetLocation)).Sum();
+            }
+            else
+            {
+                fuel = crabLocations.Select(o => Enumerable.Range(0, Math.Abs(o - targetLocation) + 1).Sum()).Sum();
+            }
+
+            return fuel;
         }
     }
 }
